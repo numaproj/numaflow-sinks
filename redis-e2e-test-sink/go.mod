2cb8a496e60877142b820698edf09ed60161970f
Keran Yang <yangkr920208@gmail.com>
2022-12-20 19:30:08 -0500

    chore: append uuid to make each message a new key in redis
    Signed-off-by: Keran Yang <yangkr920208@gmail.com>

--- redis-e2e-test-sink/go.mod
+++ redis-e2e-test-sink/go.mod
@@ -4,6 +4,7 @@ go 1.18
 
 require (
 	github.com/go-redis/redis/v8 v8.11.4
+	github.com/google/uuid v1.1.2
 	github.com/numaproj/numaflow-go v0.3.0
 )
 
--- redis-e2e-test-sink/main.go
+++ redis-e2e-test-sink/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"github.com/google/uuid"
 	"log"
 	"time"
 
@@ -24,12 +26,15 @@ func handle(ctx context.Context, datumStreamCh <-chan sinksdk.Datum) sinksdk.Res
 
 		// Our E2E tests time out after 10 minutes. Set redis message TTL to the same.
 		const msgTTL = 10 * time.Minute
-		err := client.Set(ctx, string(d.Value()), 1, msgTTL).Err()
+		// When redis sink receives two datum with same data, e2e tests should be able to verify two occurrences.
+		// We append an uuid to the datum, to make the key globally unique. Such that two datum with same data get persisted in redis as two separate keys.
+		key := fmt.Sprintf("%s-%s", string(d.Value()), uuid.New().String())
+		err := client.Set(ctx, key, 1, msgTTL).Err()
 
 		if err != nil {
 			log.Println("Set Error - ", err)
 		} else {
-			log.Printf("Added key %s\n", string(d.Value()))
+			log.Printf("Added key %s\n", key)
 		}
 
 		id := d.ID()
